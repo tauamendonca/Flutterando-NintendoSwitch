990cb87ab0d2e074ac3e7d94cc8992bf12dd6249
Tauã Mendonça <tauamendonca@gmail.com>
2021-12-21 16:27:55 -0300

    Building Analogic Joystick Button

--- lib/components/ninSwitch/bgSwitch/bg_ninswitch.dart
+++ lib/components/ninSwitch/bgSwitch/bg_ninswitch.dart
@@ -1,8 +1,5 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-//import 'package:ninswitchfteam/components/ninSwitch/buttons/digitalDirectional/digital_joypad.dart';
-import 'package:ninswitchfteam/components/ninSwitch/gameScreen/game_screen.dart';
+import 'package:ninswitchfteam/components/ninSwitch/buttons/analogJoypad/analog_joypad.dart';
 
 class BgNinswitch extends StatefulWidget {
   const BgNinswitch({Key? key}) : super(key: key);
@@ -11,41 +8,29 @@ class BgNinswitch extends StatefulWidget {
   _BgNinSwitch createState() => _BgNinSwitch();
 }
 
-//aspect ratio - manter o ratio
-//wrapper - custom widget (um container ou sizedbox) com parametros pré-definidos
-//componentização no Flutter
 class _BgNinSwitch extends State<BgNinswitch> {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
         return Center(
-          child: AspectRatio(
-            aspectRatio: 9 / 16,
-            child: Container(
-              height: constraints.maxHeight * 0.92,
-              width: constraints.maxWidth * 0.92,
-              constraints: const BoxConstraints(
-                  minWidth: 375,
-                  maxWidth: 1080,
-                  minHeight: 667,
-                  maxHeight: 1920),
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color(0xff4B5054),
-                    Color(0xff272B2E),
-                  ],
-                ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20),
-                ),
+          child: Container(
+            height: constraints.maxHeight,
+            width: constraints.maxWidth,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xff4B5054),
+                  Color(0xff272B2E),
+                ],
               ),
-              //child: const GameScreen(),
-              child: GameScreen(),
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
+              ),
             ),
+            child: const AnalogJoypad(),
           ),
         );
       },
--- lib/components/ninSwitch/buttons/analogJoypad/analog_joypad.dart
+++ lib/components/ninSwitch/buttons/analogJoypad/analog_joypad.dart
@@ -14,19 +14,47 @@ class _AnalogJoypadState extends State<AnalogJoypad> {
       return Container(
           width: constraints.maxHeight * 0.2218,
           height: constraints.maxHeight * 0.2218,
-          decoration:
-              const BoxDecoration(color: Colors.green, shape: BoxShape.circle),
-          child: Container(
-              width: constraints.maxHeight * 0.812,
-              height: constraints.maxHeight * 0.812,
-              decoration: const BoxDecoration(
-                  color: Colors.green, shape: BoxShape.circle),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xff686D70), Color(0xff050F11)]),
+            border: Border.all(
+              color: Colors.black,
+              width: 4,
+            ),
+          ),
+          child: FractionallySizedBox(
+              widthFactor: 0.9236,
+              heightFactor: 0.9236,
               child: Container(
-                width: constraints.maxHeight * 0.9236,
-                height: constraints.maxHeight * 0.9236,
-                decoration: const BoxDecoration(
-                    color: Colors.yellow, shape: BoxShape.circle),
-              )));
+                  decoration: const BoxDecoration(
+                      color: Colors.green, shape: BoxShape.circle),
+                  child: FractionallySizedBox(
+                    widthFactor: 0.9236,
+                    heightFactor: 0.9236,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                      child: FractionallySizedBox(
+                        widthFactor: 0.9236,
+                        heightFactor: 0.9236,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 4,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ))));
     });
   }
 }
